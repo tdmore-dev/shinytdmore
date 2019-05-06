2e4f408ad43fb59914e7b5e9b49fe9905576b7c3
Nicolas Luyckx <nicolas.luyckx@sgs.com>
2019-05-06 12:42:47 +0200

    Remove annoying warning: 'Warning: Can only have one: config'.

--- R/plotUtilities.R
+++ R/plotUtilities.R
@@ -96,8 +96,8 @@ getMeasureColumnLabel <- function(model, breakLine=T) {
 #'
 mergePlots <- function(p1, p2) {
   subplot(
-    ggplotly(p1) %>% config(scrollZoom=T, displayModeBar=F, displaylogo=F),
-    ggplotly(p2) %>% config(scrollZoom=T, collaborate=F, displayModeBar=F, displaylogo=F),
+    ggplotly(p1) %>% config(scrollZoom=T, displayModeBar=F, displaylogo=F, collaborate=F),
+    ggplotly(p2) %>% config(scrollZoom=T, displayModeBar=F, displaylogo=F, collaborate=F),
     nrows = 2, heights = c(0.8, 0.2), widths = c(1), shareX=T, shareY=F, titleX=T, titleY=T
   ) %>% layout(dragmode = "pan")
 }
