33428dddbf7bcb3da9aedc873edccc3ee7f5d1b9
rfaelens <ruben.faelens@kuleuven.be>
2019-12-10 16:27:46 +0100

    Better organisation of R files

---
+++ R/ui_aboutTab.R
@@ -0,0 +1,31 @@
+#' This tab shows general information out the application.
+#'
+#' @param id namespace id
+#' @param htmlFile the html file to use for the contents of the about tab. If missing, we use the default about.html from the package directory.
+#' 
+#' @return a tabPanel with HTML content
+#' @export
+#' 
+aboutTabUI <- function(id, htmlFile) {
+  ns <- NS(id)
+  if(missing(htmlFile)) htmlFile <- system.file(package="shinytdmore", "about.html")
+  htmlContent <- readLines(htmlFile, warn=FALSE)
+  tabPanel(
+    "About",
+    icon = icon("question"),
+    value=id,
+    HTML(htmlContent)
+  )
+}
+
+#' About tab server.
+#'
+#' @param input shiny input
+#' @param output shiny output
+#' @param session shiny session
+#' 
+#' @export
+#' 
+aboutTab <- function(input, output, session) {
+  # Nothing to do
+}
