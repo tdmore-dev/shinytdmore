8fcd79e17c9d38a9245ebe25c254a33ae459beb2
rfaelens <ruben.faelens@kuleuven.be>
2020-02-07 13:05:52 +0100

    ui_now should not override state$now

--- R/ui_now.R
+++ R/ui_now.R
@@ -12,15 +12,29 @@ nowInputUI <- function(id) {
 #' @param output output object
 #' @param session session object
 #' @param state reactiveValues object with value `now`. The server logic ensures a synchronization between `state$now` and the nowInput element.
-nowInput <- function(input, output, session, state) {
+#' @param debug whether to print debugging messages
+nowInput <- function(input, output, session, state, debug=FALSE) {
   # synchronize input$now to state$now
   format="%Y-%m-%d %H:%M" # format: "2000-01-01 00:00"
+  log <- function(...) if(debug) cat(...)
   observeEvent(state$now, {
-    value <- strftime(state$now, format=format)
-    if(!is.na(value) && !isTRUE(all.equal(input$now, value))) updateEditableInput(session, "now", value)
+    value <- strftime(lubridate::round_date(state$now, unit="minute"), format=format)
+    log("Updating FROM STATE to HTML (value=", value, "...")
+    if(!is.na(value) && !isTRUE(all.equal(input$now, value))) {
+      updateEditableInput(session, "now", value)
+      log("UPDATED\n")
+    } else {
+      log("IGNORED\n")
+    }
   })
   observeEvent(input$now, {
     value <- as.POSIXct(input$now, format=format)
-    if(!is.na(value) && !isTRUE(all.equal(state$now, value))) state$now <- value
-  })
+    log("Updating FROM HTML to STATE (value=", value, "...")
+    if(!is.na(value) && !isTRUE(all.equal(lubridate::round_date(state$now, unit="minute"), value))) {
+      state$now <- value
+      log("UPDATED\n")
+    } else {
+      log("IGNORED\n")
+    }
+  }, ignoreInit=TRUE) #do not synchronize when just starting; updateEditableInput does not modify input$now in the same service loop
 }
