c001ea74eb867defd757d79df9c5d5f500b4e65e
rfaelens <ruben.faelens@kuleuven.be>
2020-02-10 16:12:34 +0100

    Ensure plotly is fully loaded before snapshot

--- tests/shinytest/testPlots/tests/test-plots.R
+++ tests/shinytest/testPlots/tests/test-plots.R
@@ -3,8 +3,15 @@ library(shinytest)
 app <- ShinyDriver$new("..", loadTimeout = 30000, seed=1234)
 app$snapshotInit("test-plots")
 source("../../../testthat/helperShinytest.R")
-
 app$waitFor("$('.recalculating').length == 0")
+
+## It takes a while for the plot to render
+## during this rendering, plotly sets the attribute
+## .clientValue-default-plotlyCrosstalkOpts
+## See https://github.com/ropensci/plotly/blob/bcae42bbeea8d8368fb3ca881243e6acd0c661b4/inst/htmlwidgets/plotly.js#L35
+## And https://github.com/rstudio/crosstalk/blob/68b0b617ee82e5d6b738f26106933254ce5ede53/R/crosstalk.R#L97
+## and https://github.com/rstudio/crosstalk/blob/master/javascript/src/var.js#L37
+app$waitFor("$('.plotly .plot-container .svg-container').length > 0")
 
 ## snapshot population plot
 app$snapshot(filename="population.json")
