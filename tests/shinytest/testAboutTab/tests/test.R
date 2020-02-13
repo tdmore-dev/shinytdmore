2b87a73648f4a2ed06fc9ba2a7c2a4607d914d93
rfaelens <ruben.faelens@kuleuven.be>
2020-02-13 13:33:58 +0000

    Adapt tests

--- tests/shinytest/testAboutTab/tests/test.R
+++ tests/shinytest/testAboutTab/tests/test.R
@@ -12,7 +12,8 @@ app$snapshot(filename="snapshot.json")
 el <- app$findElement(xpath="//div[@data-value='aboutTabId']")
 el$findElement(xpath="p")$getText()
 
-snapshotSource("source") #make sure downloaded HTML matches
+## The snapshot will contain the build dates of each package, and will *constantly* change
+##snapshotSource("source") #make sure downloaded HTML matches
 
 p <- app$.__enclos_env__$private$shinyProcess
 p$interrupt()
