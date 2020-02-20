45de27b5076ff70c7ddfa1c4e1501045ebf0334b
rfaelens <ruben.faelens@kuleuven.be>
2020-02-20 17:48:46 +0100

    CI: Ensure we wait long enough on slow systems

--- tests/shinytest/testPredictionTab/tests/test-predictiontab.R
+++ tests/shinytest/testPredictionTab/tests/test-predictiontab.R
@@ -14,7 +14,7 @@ el$click(); Sys.sleep(1) #wait until animation completes
 
 app$setInputs(`prediction-covariates-add`="click", values_=FALSE)
 
-app$waitFor("$('#prediction-covariates-table-table .handsontable td').length == 3")
+app$waitFor("$('#prediction-covariates-table-table .handsontable td').length == 3", timeout=10*1000)
 cells <- app$findElements("#prediction-covariates-table-table .handsontable td")
 cells[[1]]$click(); cells[[1]]$setValue("2000-01-02")
 cells[[2]]$click(); cells[[2]]$setValue("08:00")
@@ -24,7 +24,7 @@ app$takeScreenshot()
 # add a dose
 app$setInputs(`prediction-doses-add`="click", values_=FALSE) #do not request values_; fails because of plotly output / shiny::req issue
 
-app$waitFor("$('#prediction-doses-table-table .handsontable td').length == 5")
+app$waitFor("$('#prediction-doses-table-table .handsontable td').length == 5", timeout=10*1000)
 cells <- app$findElements("#prediction-doses-table-table .handsontable td")
 cells[[1]]$click(); cells[[1]]$setValue("2000-01-02")
 cells[[2]]$click(); cells[[2]]$setValue("08:00")
